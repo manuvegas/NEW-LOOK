d6e85a7835cd483d9d0ff3563860e9c9d125b8b1
manu <vegasemanuel@hotmail.com>
2024-05-05 11:07:56 -0300

    NewLook

--- src/components/Pages/Contactos.jsx
+++ src/components/Pages/Contactos.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import  Input  from "../UI/input";
-import { Textarea } from "../UI/Textarea";
+import { Textarea } from "../UI/textarea";
 
 function Contactos() {
   const location = useLocation();
